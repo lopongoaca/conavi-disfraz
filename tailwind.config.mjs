dc9057f48b432f2a892a343f24fcfcfad34135aa
lopongoaca <contacto@lopongoaca.com>
2023-10-19 09:16:31 -0500

    Cambiar abejita por tarjeta

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundColor: (theme) => ({
+				'pattern': '#FFF'
+			}),
 			backgroundImage: (theme) => ({
 				'pattern': "url('/trama.png')"
 			}),
